35892e7079ddf41c7c4bdf8dcbc2fce7a9f5ac7d
yassir <muchamad445@gmail.com>
2022-05-09 22:05:57 +0700

    update models

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+# from django.core.urlresolvers import reverse
 
 # Create your models here.
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,self).get_queryset().filter(status='published')
+        return super().get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = {
@@ -29,3 +31,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+            args=[self.publish.year,
+            self.publish.strftime('%m'),
+             self.publish.strftime('%d'), self.slug])
