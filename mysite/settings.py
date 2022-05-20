c4a4ec3e2dfacc25ca0b4a6266227ba91b20249a
yassir <muchamad445@gmail.com>
2022-05-20 08:48:50 +0700

    update comment

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'yasir.bpras@gmail.com'
-EMAIL_HOST_PASSWORD = 'althaffanisa'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
